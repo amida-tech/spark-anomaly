a0c958b588978efdfea8224a1143690bbc57270c
hopeemac <hope.e.mcintyre@gmail.com>
2017-08-16 17:00:44 -0400

    Running code with distance calculation to each cluster

--- app_dataframe.py
+++ app_dataframe.py
@@ -7,8 +7,8 @@
 ## Imports
 # from pyspark import SparkConf, SparkContext
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import *
-from pyspark.sql.types import *
+from pyspark.sql.functions import desc
+from pyspark.sql.types import IntegerType
 
 # Clustering
 from pyspark.ml.clustering import KMeans
@@ -16,24 +16,14 @@ from pyspark.ml.feature import VectorAssembler
 # Note: Have to install numpy in python
 from scipy.spatial import distance
 
+import math
+import numpy
+
 ## Module Constantscd
 APP_NAME = "Log Cluster"
 FILE_NAME = "./kdd-data/kddcup.data.txt"
 
 ## Closure Functions
-def parse(row):
-    """
-    Parses a row and returns a named tuple.
-    """
-
-    row[0]  = datetime.strptime(row[0], DATE_FMT).date()
-    row[5]  = datetime.strptime(row[5], TIME_FMT).time()
-    row[6]  = float(row[6])
-    row[7]  = datetime.strptime(row[7], TIME_FMT).time()
-    row[8]  = float(row[8])
-    row[9]  = float(row[9])
-    row[10] = float(row[10])
-    return Flight(*row[:11])
 
 def cast_columns(spark_df, column_list):
     """
@@ -45,15 +35,43 @@ def cast_columns(spark_df, column_list):
         # spark_df = spark_df.select(column).cast(IntegerType()))
     return spark_df
 
-def dist_to_centroid(feature_vector, model):
-    cluster_number = model.predict(feature_vector)
-    print(cluster_number)
-    centroid = model.clusterCenters(cluster_number)
-    print(centroid)
-    dist = distance.euclidean(feature_vector,centroid)
-    return(dist)
+def dist_to_centroid(point_vector, model, k):
+    distances = []
+    for cluster_number in range(k):
+        centroid = model.clusterCenters(cluster_number)
+        dist = distance.euclidean(point_vector,centroid)
+        distances.append(dist)
+        return(distances)
 
+def min_dist_to_centroid(point_vector, centers):
+    distances = []
+    for center_point in centers:
+        # dist = distance.euclidean(point_vector, center_point)
+        dist = calculate_euclidean_distance(point_vector, center_point)
+        distances.append(dist)
+    min_distance = min(distances)
+    # return(min_distance)
+    return min_distance
 
+def map_test(point_vector, text):
+    # print(point_vector)
+    return 2
+
+def row_to_list(row):
+    columns = row.__fields__
+    list_format = []
+    for column in columns:
+        list_format.append(row[column])
+    return list_format
+
+def calculate_euclidean_distance(vector1, vector2):
+    '''calculate the euclidean distance, no numpy
+    input: numpy.arrays or lists
+    return: euclidean distance
+    '''
+    dist = [(a - b)**2 for a, b in zip(vector1, vector2)]
+    dist = math.sqrt(sum(dist))
+    return dist
 
 ## Main functionality
 def main(sparkSession):
@@ -111,23 +129,27 @@ def main(sparkSession):
 
     # Shows the result
     centers = model.clusterCenters()
-    print("Cluster Centers: ")
-    for center in centers:
-        print(center)
+    centers = [center.tolist() for center in centers]
+    print(len(centers))
 
     # Find anommaly amongst original dataset
     threshold = 0.8
     # dist = dist_to_centroid(feature_vector, model)
 
-    row_list = cluster_dataset.select('features').collect()
-    feature_vectors = [row.features for row in row_list]
-    print(len(feature_vectors))
+    #row_list = cluster_dataset.select('features').collect()
+    #feature_vectors = [row.features for row in row_list]
+    #print(len(feature_vectors))
+    #print(type(feature_vectors[0]))
 
-    x = sparkSession.sparkContext.parallelize(feature_vectors)
-    distances = x.map(dist_to_centroid, model)
-    # print(len(distances.collect()))
+    feature_vectors = dataset.rdd.map(row_to_list) #rdd
+    min_distances = feature_vectors.map(lambda x: min_dist_to_centroid(x, centers)) #rdd
+    min_distances_list = min_distances.collect() # list of values
+    print(min_distances_list[0:10])
+    # distances_list = distances.collect()
+    # min_distances = [min(distance_list) for distance_list in distances_list]
+    # print(min_distances[0:100])
 
-    print(distances)
+
 
 
 if __name__ == "__main__":
